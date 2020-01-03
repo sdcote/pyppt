17b747b9da155920e89287c4047550efad61f069
sdcote <sdcote@aep.com>
2020-01-03 10:23:57 -0500

    Streamline single line IFs, added method calls for optional fields

--- create.py
+++ create.py
@@ -8,6 +8,16 @@ from pptx import Presentation
 CONCEPT = "Concept"
 
 
+def add_note_to_slide(slide, slide_notes):
+    notes_slide = slide.notes_slide
+    text_frame = notes_slide.notes_text_frame
+    text_frame.text = slide_notes
+
+
+def add_background_image_to_slide(slide, slide_bkgnd):
+    pass
+
+
 def create_deck(titletxt, subtitletxt, name):
     prs = Presentation('template.pptx')
     title_slide_layout = prs.slide_layouts[0]
@@ -21,35 +31,31 @@ def create_deck(titletxt, subtitletxt, name):
         reader = csv.reader(f)
         my_list = list(reader)
 
+        slide_notes = None
+        slide_bkgnd = None
+
         for l in my_list:
-            if len(l) == 0:
-                continue
+            if len(l) == 0: continue
 
             layout = l[0]
-            if layout is None:
-                layout = CONCEPT
+            if layout is None: layout = CONCEPT
             slide_title = l[1]
             slide_body = l[2]
-            if len(l) > 3:
-                slide_notes = l[3]
-            else:
-                slide_notes = ""
+            if len(l) > 3: slide_notes = l[3]
+            if len(l) > 4: slide_bkgnd = l[4]
 
             blank_slide_layout = prs.slide_layouts.get_by_name(layout)
             slide = prs.slides.add_slide(blank_slide_layout)
             title = slide.shapes.title
             title.text = slide_title
             shapes = slide.shapes
-            if layout is not "Section Header":
-                body_shape = shapes.placeholders[1]
-                tf = body_shape.text_frame
-                tf.text = slide_body
-            else:
-                print("Argh: " + slide_body)
+            body_shape = shapes.placeholders[1]
+            tf = body_shape.text_frame
+            tf.text = slide_body
 
-            notes_slide = slide.notes_slide
-            text_frame = notes_slide.notes_text_frame
-            text_frame.text = slide_notes
+            if slide_notes is not None: add_note_to_slide(slide, slide_notes)
+
+            if slide_bkgnd is not None: add_background_image_to_slide(slide, slide_bkgnd)
 
     prs.save(name + '.pptx')
 
